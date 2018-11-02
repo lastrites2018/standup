91bd33e24bc03c1d92ea03077e15536d9a811190
lastrites2018 <lastrites@nate.com>
2018-11-02 17:17:30 +0900

    Updates

---
+++ precache-manifest.dacbcf5b3119875874e4736b07adbbdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/standup/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "053d9693c5023b2b1f72",
+    "url": "/standup/static/js/runtime~main.053d9693.js"
+  },
+  {
+    "revision": "3b0e365bb38802f68d4e",
+    "url": "/standup/static/js/main.3b0e365b.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/standup/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "3b0e365bb38802f68d4e",
+    "url": "/standup/static/css/main.adf7d7f3.chunk.css"
+  },
+  {
+    "revision": "5e868a3f4b6732b1714b26773cff5b93",
+    "url": "/standup/index.html"
+  }
+];
